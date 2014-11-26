5ce7c4c0af0a94ea5bfe7b589457541e3949b9b7
FWorku <FWorku@cs_labv-202.cs.mum.edu>
2014-11-26 11:39:59 -0600

    multiplicity added

--- src/main/java/edu/mum/cs544/model/Prescription.java
+++ src/main/java/edu/mum/cs544/model/Prescription.java
@@ -7,10 +7,15 @@ package edu.mum.cs544.model;
 
 import java.io.Serializable;
 import java.util.Date;
+import java.util.List;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.OneToMany;
 
 /**
  *
